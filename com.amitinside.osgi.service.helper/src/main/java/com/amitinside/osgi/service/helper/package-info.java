ab4a1e38d2c6e33c384ea64890ace18522b6168e
Amit Kumar Mondal <admin@amitinside.com>
2018-01-19 00:36:19 +0100

    Added OSGi Package Versiong Info

--- com.amitinside.osgi.service.helper/src/main/java/com/amitinside/osgi/service/helper/package-info.java
+++ com.amitinside.osgi.service.helper/src/main/java/com/amitinside/osgi/service/helper/package-info.java
@@ -1,8 +1,20 @@
-/**
- * 
- */
-/**
- * @author Amit Kumar Mondal
+/*******************************************************************************
+ * Copyright (c) 2018 Amit Kumar Mondal
  *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+/**
+ * Provides Service Helper Class
+ * <p>
+ * {@code  Import-Package: com.amitinside.osgi.service.helper;version="[1.0,2.0)"}
+ *
+ * @since 1.0
  */
+@Version("1.0")
 package com.amitinside.osgi.service.helper;
+
+import aQute.bnd.annotation.Version;
